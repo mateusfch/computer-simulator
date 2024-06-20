dd07c02dfe87c2481915f07ffd9b289f594d5748
mateusfch <charloto.mateus@gmail.com>
2024-06-20 15:26:37 -0300

    funções: instructionExe, execution e simulateComputer

--- trab1.hs
+++ trab1.hs
@@ -9,7 +9,8 @@ type Memory = [(Address, Value)] -- 0 a 255 (teoricamente)
 data CPU = CPU {
     acc :: Value,   -- Acumulador
     pc :: Address,  -- Program Counter
-    eqz :: Bool     -- Flag EQZ
+    eqz :: Bool,     -- Flag EQZ
+    instructionRegister :: (Int, Int) -- Registrador de instrucoes
 } deriving (Show)
 
 -- instruções possíveis
@@ -33,9 +34,6 @@ fetchValue addr mem =
         Just val -> val
         Nothing -> error "Endereco de memoria nao encontrado"
 
--- lod: Carrega o conteúdo do endereço de memória <end> no registrador acumulador (ACC).
-lod :: Address -> Memory -> CPU -> CPU
-lod addr mem cpu = cpu {acc = fetchValue addr mem}
 
 -- sto: Armazena o conteúdo do registrador acumulador (ACC) no endereço de memória <end>.
 sto :: Address -> Memory -> CPU -> Memory
@@ -46,31 +44,109 @@ sto addr ((addr_t,val_t):xs) cpu
 
 
 
+instructionExe :: (Int, Int) -> Memory -> CPU -> (Memory, CPU)
+instructionExe (opcode, addr) mem cpu = 
+    case opcode of 
+        2 -> (mem, cpu {acc = fetchValue addr mem})
+        4  -> (sto addr mem cpu, cpu)
+        6  -> (mem, cpu {pc = addr})
+        8  -> if eqz cpu then (mem, cpu {pc = addr}) else (mem, cpu {pc = pc cpu + 2})
+        10 -> if acc cpu == fetchValue addr mem then (mem, cpu {acc = 0, eqz = True}) else (mem, cpu {acc = 1, eqz = False})        14 -> cpu {acc = fetchValue addr mem + acc cpu}
+        14 -> (mem, cpu {acc = acc cpu + fetchValue addr mem})
+        18 -> (mem, cpu {acc = acc cpu * fetchValue addr mem})
+        20 -> (mem, cpu)  
+
+
+-- execution: para carregar o registrador de instruções e atualizar o contador de instruções
+execution :: Memory -> CPU -> CPU
+execution mem cpu = 
+    let 
+    instr = (fetchValue(pc cpu) mem, fetchValue(pc cpu +1) mem)
+    in cpu {instructionRegister = instr, pc = pc cpu + 2}
+
+
+simulateComputer :: Memory -> CPU -> CPU 
+simulateComputer mem cpu = 
+   -- primeiramente, eh preciso carregar o registrador de instrucoes com a instrucao a ser executada
+    let cpu1 = execution mem cpu 
+        -- depois, executo a instrucao e recebo a memoria e a cpu atualizadas
+        (updatedMem, updatedCPU) = instructionExe (instructionRegister cpu1) mem cpu1
+        in if fst (instructionRegister cpu1) == 20 then updatedCPU else simulateComputer updatedMem, updatedCPU
+
+
+-- o programa vai ser carregado previamente na memoria e o contador vai ser responsavel por avançando as instrucoes
+
+
+-- mem = [(0,2), (1,240), (2,3), (240,10)]
+-- while pc != 20: 
+--     pc <- pc + 2
+--     registrador_instrucoes <- (0,240)
+--     executa_instrucao registrador_instrucoes 
+
+
+-- -- Função para testar o programa
+-- main :: IO ()
+-- main = do
+--     -- Exemplo de memória inicial
+--     let mem = [
+--                 (0, 2), (1, 240),  -- LOD 240
+--                 (2, 16), (3, 241), -- MUL 241
+--                 (4, 4), (5, 251),  -- STO 251
+--                 (6, 20),           -- HLT
+--                 (240, 10),         -- Valor de A
+--                 (241, 20)          -- Valor de B
+--                ]
+--         cpu = CPU {acc = 0, pc = 0, eqz = False}
+--         finalCPU = simulate mem cpu
+--     print finalCPU
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- DEFINICAO DAS INSTRUCOES COMO FUNCOES
+
+-- -- lod: Carrega o conteúdo do endereço de memória <end> no registrador acumulador (ACC).
+-- lod :: Address -> Memory -> CPU -> CPU
+-- lod addr mem cpu = cpu {acc = fetchValue addr mem}
+
+
+
+
 -- jmp: Desvio incondicional: carrega no contador de instruções o valor <end> forçando com que a próxima instrução
 -- a ser executada seja a que se encontra no endereço de memória <end>
-jmp :: Address -> CPU -> CPU 
-jmp addr cpu = cpu{pc = addr}
+-- jmp :: Address -> CPU -> CPU 
+-- jmp addr cpu = cpu{pc = addr}
 
--- jmz: Desvio condicional: funcionamento análogo ao da instrução JMP com a diferença que a carga do contador de instruções 
--- só ocorre se o valor do acumulador for igual a zero (de acordo com a flag EQZ).
-jmz :: Address -> CPU -> CPU 
-jmz addr cpu = if eqz cpu then cpu{pc = addr} else cpu{pc = pc cpu + 2}
+-- -- jmz: Desvio condicional: funcionamento análogo ao da instrução JMP com a diferença que a carga do contador de instruções 
+-- -- só ocorre se o valor do acumulador for igual a zero (de acordo com a flag EQZ).
+-- jmz :: Address -> CPU -> CPU 
+-- jmz addr cpu = if eqz cpu then cpu{pc = addr} else cpu{pc = pc cpu + 2}
 
--- add: Adiciona o conteúdo do endereço de memória <end> ao conteúdo armazenado no acumulador (ACC) e armazena a resposta no próprio acumulador.
-add :: Address -> Memory -> CPU -> CPU
-add addr mem cpu = cpu {acc = fetchValue addr mem + acc cpu }
+-- -- add: Adiciona o conteúdo do endereço de memória <end> ao conteúdo armazenado no acumulador (ACC) e armazena a resposta no próprio acumulador.
+-- add :: Address -> Memory -> CPU -> CPU
+-- add addr mem cpu = cpu {acc = fetchValue addr mem + acc cpu }
 
--- sub: Subtrai o conteúdo do endereço de memória <end> do conteúdo do acumulador (ACC) e armazena a resposta no próprio acumulador.
-sub :: Address -> Memory -> CPU -> CPU
-sub addr mem cpu = cpu {acc = fetchValue addr mem - acc cpu }
+-- -- sub: Subtrai o conteúdo do endereço de memória <end> do conteúdo do acumulador (ACC) e armazena a resposta no próprio acumulador.
+-- sub :: Address -> Memory -> CPU -> CPU
+-- sub addr mem cpu = cpu {acc = fetchValue addr mem - acc cpu }
 
--- cpe: Se o conteúdo do endereço <end> for igual ao acumulador, coloca 0 no acumulador, caso contrário coloca 1
-cpe :: Address -> Memory -> CPU -> CPU
-cpe addr mem cpu = if acc cpu == fetchValue addr mem then cpu{acc=0, eqz=True} else cpu{acc=1, eqz=False}
+-- -- cpe: Se o conteúdo do endereço <end> for igual ao acumulador, coloca 0 no acumulador, caso contrário coloca 1
+-- cpe :: Address -> Memory -> CPU -> CPU
+-- cpe addr mem cpu = if acc cpu == fetchValue addr mem then cpu{acc=0, eqz=True} else cpu{acc=1, eqz=False}
 
-
--- nop: Não executa ação nenhuma (No OPeration).
-nop :: CPU -> CPU 
-nop cpu = cpu{pc = pc cpu + 2}
-
--- hlt: Encerra o ciclo de execução do processador (HaLT)
+-- -- nop: Não executa ação nenhuma (No OPeration).
+-- nop :: CPU -> CPU 
+-- nop cpu = cpu{pc = pc cpu + 2}
