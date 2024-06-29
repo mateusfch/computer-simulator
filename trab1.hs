2d727f26c10ac2cdf5a034c07a2e7815a7359e47
mateusfch <charloto.mateus@gmail.com>
2024-06-28 23:37:50 -0300

    fix: Caso 3

--- trab1.hs
+++ trab1.hs
@@ -7,10 +7,10 @@ type Memory = [(Address, Value)] -- 0 a 255 (teoricamente)
 
 -- estrutura de dados que representa o estado da CPU
 data CPU = CPU {
-    acc :: Value,   -- Acumulador
-    pc :: Address,  -- Program Counter
-    eqz :: Bool,     -- Flag EQZ
-    instructionRegister :: (Int, Int) -- Registrador de instrucoes
+    acc :: Value,   
+    pc :: Address,  
+    eqz :: Bool,     
+    instructionRegister :: (Int, Int) 
 } deriving (Show)
 
 -- fetchValue: encontra o valor que esta no endereco de memoria especificado
@@ -61,6 +61,8 @@ simulateComputer mem cpu =
         (updatedMem, updatedCPU) = instructionExe (instructionRegister cpu1) mem cpu1
         in if fst (instructionRegister cpu1) == 20 then (updatedMem, updatedCPU) else simulateComputer updatedMem updatedCPU
 
+
+
 -- Função para testar o programa
 main :: IO ()
 main = do
@@ -68,71 +70,89 @@ main = do
     -- Exemplo (1) 
     -- Resp = A + B – 2
     
-    -- let mem = [(0,2),(1,240),
-    --             (2,14),(3,241),
-    --             (4,16), (5,242),
-    --             (6,4),(7,251),
-    --             (8,20),(9,18),
-    --             (240,100),
-    --             (241,50),
-    --             (242,2),
-    --             (251,0)]
+    -- let mem = [
+    --             (0,2),(1,240), -- lod: carrega o valor de A no acc
+    --             (2,14),(3,241), -- add: soma ao acc o valor de B
+    --             (4,16), (5,245), -- sub: subtrai do acc a cte 2
+    --             (6,4),(7,251), -- sto: carrega em resp o valor do acc
+    --             (8,20),(9,18), -- hlt: encerra o ciclo de execucao do processador
+    --             (240,100), -- A
+    --             (241,50), -- B
+    --             (245,2), -- valor cte 2
+    --             (251,0), -- resp
+    --             (252,0), -- endereco de video
+    --             (253,0), -- endereco de video
+    --             (254,0), -- endereco de video
+    --             (255,0) -- endereco de video
+    --             ] -- endereco de video
     
     -- Exemplo (2)
     -- Resp = A * B;
 
-    let mem = [
-                (0, 2), (1, 240), -- armazenei o valor de A no acumulador
-                (2, 8), (3,24), -- 0 * B --> resp=0,
-                (4,2), (5,241), -- armazenei o valor de B no acumulador
-                (6,8), (7,24), -- A * 0 --> resp = 0
+    -- let mem = [
+    --             (0, 2), (1, 240), -- lod: armazenei o valor de A no acumulador
+    --             (2, 8), (3,24), -- jmz: 0 * B --> resp=0,
+    --             (4,2), (5,241), -- lod: armazenei o valor de B no acumulador
+    --             (6,8), (7,24), -- jmz: A * 0 --> resp = 0
 
-                (8,16), (9,246), --  subtrai uma unidade do acumulador 
-                (10, 4), (11, 242), -- carreguei no end. 242 o estado do acumulador subtraido
-                (12, 2), (13, 251), -- carrego o valor atual de resp no acumulador
-                (14,14), (15,240),   -- somo A no acumulador
-                (16, 4), (17, 251), -- carrego o resultado da soma em resp
+    --             (8,16), (9,246), -- sub: subtrai uma unidade do acumulador 
+    --             (10, 4), (11, 242), -- sto: carrego no end. 242 o estado do acumulador subtraido
+    --             (12, 2), (13, 251), -- lod: carrego o valor atual de resp no acumulador
+    --             (14,14), (15,240),   -- add: somo A no acumulador
+    --             (16, 4), (17, 251), -- sto: carrego o resultado da soma em resp
                 
-                (18, 2), (19, 242), -- carrego no acumulador o valor de B sendo subtraido
-                (20, 8), (21,24), -- desvio (jmz) pro fim se acumulador = 0
-                (22, 6), (23,8), -- desvio (jmp) pro inicio pra uma nova iteracao 
-                (24, 20), (25,18),
+    --             (18, 2), (19, 242), -- lod: carrego no acumulador o valor de B sendo subtraido
+    --             (20, 8), (21,24), -- jmz: desvio (jmz) pro fim se acumulador = 0
+    --             (22, 6), (23,8), -- jmp: desvio (jmp) pro inicio pra uma nova iteracao 
+    --             (24, 20), (25,18), -- hlt: encerra o ciclo de execucao do processador
                 
-                (240,2), -- A
-                (241,2), -- B
-                (242,0), -- B sendo subtraido
-                (246, 1), -- cte 1
-                (251,0) -- resp
-            ]
+    --             (240,2), -- A
+    --             (241,2), -- B
+    --             (242,0), -- B sendo subtraido
+    --             (246, 1), -- cte 1
+    --             (251,0), -- resp
+    --             (252,0), -- endereco de video
+    --             (253,0), -- endereco de video
+    --             (254,0), -- endereco de video
+    --             (255,0) -- endereco de video
+    --         ]
    
     
     -- Exemplo (3)
     -- A = 0; Resp = 1; while(A < 5) { A = A + 1; Resp = Resp + 2; }
-    -- let mem = [
-    --               (0, 2), (1,240),
-    --               (2, 14), (3, 245),
-    --               (4,4), (5,240),
+    let mem = [
+                  (0, 2), (1,240), -- lod: carrega o valor de A no acc
+                  (2, 10), (3,241), -- cpe: A == 5 ? acc=0:1
+                  (4, 8), (5, 20), -- jmz: acc==0 --> fim
+                  (6, 2), (7,240), -- lod: carrega o valor de A no acc
+                  (8, 14), (9, 245), -- jmz: soma +1 ao acc
+                  (10,4), (11,240), -- sto: carrega o valor de acc em A
                   
-    --               (6,2), (7, 251),
-    --               (8,14), (9,246),
-    --               (10, 4), (11, 251),
+                  (12,2), (13, 251), -- lod: carrega o valor de resp no acc
+                  (14,14), (15,246), -- add: soma +2 ao acc
+                  (16, 4), (17, 251), -- sto: carrega o valor de acc em resp
 
-    --               (12, 2), (13, 241),
-    --               (14, 16), (15, 245),
-    --               (16, 4), (17, 241),
+                  (18,6), (19,0), -- jmp: volta para 0, para continuar a iteracao
+                  (20, 20), (21,18), --hlt: -- encerra o ciclo de execucao
 
-    --               (18,8), (19,22),
-    --               (20, 6), (21, 0),
-    --               (22, 20), (23, 18),
-                  
-    --               (240, 0), -- a
-    --               (241, 5), -- contador do loop
-    --               (245, 1), -- valor cte que será somado ao a (a = a + 1)
-    --               (246, 2), -- valor cte que será somado ao resp
-    --               (251, 1) -- resp
-    --         ]
+                  (240, 0), -- A
+                  (241, 5), -- valor associado ao  loop
+                  (245, 1), -- valor cte que será somado ao A (A = A + 1)
+                  (246, 2), -- valor cte que será somado ao resp
+                  (251, 1), -- resp
+                  (252,0), -- endereco de video
+                  (253,0), -- endereco de video
+                  (254,0), -- endereco de video
+                  (255,0) -- endereco de video
+            ]
     let cpu = CPU {acc = 0, pc = 0, eqz = False, instructionRegister = (0,0)}
     let (finalMem, finalCPU) = simulateComputer mem cpu
-    print finalCPU
     print finalMem
+    putStrLn "TELA - Enderecos de memoria de video"
+    putStrLn $ "ENDERECO 251: " ++ show (fetchValue 251 finalMem)
+    putStrLn $ "ENDERECO 252: " ++ show (fetchValue 252 finalMem)
+    putStrLn $ "ENDERECO 253: " ++ show (fetchValue 253 finalMem)
+    putStrLn $ "ENDERECO 254: " ++ show (fetchValue 254 finalMem)
+    putStrLn $ "ENDERECO 255: " ++ show (fetchValue 255 finalMem)
+
 
